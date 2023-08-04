85ed0d74d0d08ea3b64a1d962ac6e97606745865
MaxPy22 <onlyinfo@ya.ru>
2023-08-04 20:16:37 +0300

    first frame

---
+++ oopBankomatas/oopBankomatas/Models/Client.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopBankomatas.Models
+{
+    internal class Client
+    {
+
+        public Client() { }
+        public Client(string name, string lastName)
+        {
+            Name = name;
+            LastName = lastName;
+        }
+
+        public Client(string name, string lastName, int personalID) : this (name, lastName)
+        {            
+            PersonalID = personalID;         
+        }
+
+        public Client(string name, string lastName, int personalID, int clientID) : this(name, lastName, personalID)
+        {
+            ClientID = clientID;
+        }
+
+        public Client newClientCreate() 
+        {
+            Name = "Skrudz";
+            LastName = "MacDac";
+            PersonalID = 01234567890;
+            ClientID = 03456781;
+            return new Client(); }
+
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int PersonalID { get; set; } // private
+        public int ClientID { get; set; } // private?
+
+
+
+    }
+}
--- oopBankomatas/oopBankomatas/Program.cs
+++ oopBankomatas/oopBankomatas/Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Bankomat.......!");
+
+            //DateTime today = Convert.ToDateTime(Console.ReadLine());
+            DateTime today = DateTime.Today;
+            string toText = today.ToString("yyMMdd");
+            int toInt = Convert.ToInt32(toText);
+            Console.WriteLine(toInt);
+            Console.WriteLine();
+
+
+
         }
     }
 }
