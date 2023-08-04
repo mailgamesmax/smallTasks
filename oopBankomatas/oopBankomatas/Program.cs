a80b539c871ba9ba6205d4f7231446c0408f4a9c
MaxPy22 <onlyinfo@ya.ru>
2023-08-04 19:11:35 +0300

    test first commit

---
+++ oopBankomatas/oopBankomatas/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace oopBankomatas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Bankomat.......!");
+        }
+    }
+}
